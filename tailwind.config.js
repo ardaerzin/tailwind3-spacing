56df113e997eee4a91e3bc566d7c1b985521e34a
Arda Erzin <ardaerzin@gmail.com>
2021-12-14 03:01:57 +0100

    initial

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./styles/**/*.{css}",
+  ],
+  theme: {
+    spacing: {
+      "1": "10rem",
+      "1.5": "15rem",
+    }
+  },
+  plugins: [],
+}
